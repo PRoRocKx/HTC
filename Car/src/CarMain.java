6f4fe7e57131291650c0771c8ed4ceacfb9a415f
Evgen <prorock92@yandex.ru>
2018-06-15 09:26:13 +0400

    add TextFinderadd TextFinder

--- Car/src/CarMain.java
+++
@@ -1,7 +0,0 @@
-public class CarMain {
-
-    public static void main(String... arg){
-        Car car = new Car("Car");
-        System.out.println(car.toString());
-    }
-}
--- Complex/src/ComplexMain.java
+++
@@ -1,8 +0,0 @@
-public class ComplexMain {
-    public static void main(String... arg){
-        Complex complex = new Complex(1,1);
-        System.out.println(complex.toString());
-        System.out.println(complex.toExponential());
-        System.out.println(complex.toTrigonometric());
-    }
-}
